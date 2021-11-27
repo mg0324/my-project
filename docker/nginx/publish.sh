3f15ca61148d8a18d1b555b528529313e7855b59
mango mac <1092017732@qq.com>
2021-11-27 21:33:50 +0800

    升级nginx，支持https

--- docker/nginx/publish.sh
+++ docker/nginx/publish.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # 停止并删除旧版本容器
 #ssh root@master 'docker stop nginx && docker rm nginx'
-# 运行新版本容器
-#ssh root@master 'docker run --name nginx -d --restart=always -p 80:80 -p 443:443 -v /opt/nginx/html:/usr/share/nginx/html mangomei/nginx:2.2'
 
 scp conf/* root@master:/opt/nginx/conf
 scp card.json root@master:/opt/nginx/html/config/card.json
