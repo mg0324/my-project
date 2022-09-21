17c88b55828a9f626203a2db04723d7d896ca36d
mango mac <1092017732@qq.com>
2022-09-21 16:40:23 +0800

    更新helm的readme

--- helm-charts/card-api/publish.sh
+++ helm-charts/card-api/publish.sh
@@ -5,7 +5,14 @@ mvn clean package
 # docker build and push
 docker build -t mangomei/card:latest .
 docker push mangomei/card:latest
+# 先缩容
+ssh root@master 'kubectl scale deployment card-api --replicas 0'
+sleep 5
+# 删除掉master上的mangomei/card:latest镜像
+ssh root@master 'docker image rm mangomei/card:latest'
+# 扩容到1
+ssh root@master 'kubectl scale deployment card-api --replicas 1'
 # 更新线上helm chart
 # 进入helm-chart card-api目录下
-cd /Users/mango/git/my-project/helm-charts/card-api
-helm --kubeconfig ~/.kube/k8s.yaml upgrade card-api .
+# cd /Users/mango/git/my-project/helm-charts/card-api
+# helm --kubeconfig ~/.kube/k8s.yaml upgrade card-api .
--- helm-charts/readme.md
+++ helm-charts/readme.md
@@ -22,3 +22,13 @@ helm --kubeconfig ~/.kube/rancher-local.yaml upgrade mysql-server .
 1. maven打包
 2. docker打镜像并上传
 3. helm更新
+
+### helm-chart部署集群列表
+* card-api - k8s的master节点 - 端口30901
+* es-server - 未部署
+* flask-api - docker服务器上的k3s - 端口30002
+* home-docsify - k8s的node1节点 - 端口30300
+* mysql-server - docker服务器上的k3s - 端口32306
+* nginx-card-admin - k8s的node1节点 - 端口30180
+* nginx-plan - 未部署
+* redis-server - docker服务器上的k3s - 端口30379
