d812003706ebdd78535efcd22cccae4988617614
mango mac <1092017732@qq.com>
2022-12-08 22:36:23 +0800

    修复www的https

--- home/publish.sh
+++ home/publish.sh
@@ -2,9 +2,9 @@
 # 编译打包
 yarn build
 # 删除原来部署的文件
-ssh root@mg.meiflower.top 'rm -rf /opt/nginx/html/home/*'
+ssh root@node 'rm -rf /opt/nginx/html/home/*'
 # 上传新的部署文件
-scp dist/* root@mg.meiflower.top:/opt/nginx/html/home
+scp dist/* root@node:/opt/nginx/html/home
 # 发布成功
-echo 'publish success!'
+echo 'publish success to server node!'
 
